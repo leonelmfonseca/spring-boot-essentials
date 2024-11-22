14b1e14af44108955c364b60da9e7b88b21c7d4b
leonelmfonseca <leonelfonseca@gmail.com>
2024-11-22 10:50:55 +0000

    Initial Commit

---
+++ src/test/java/com/example/ioc_container/IocContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioc_container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
